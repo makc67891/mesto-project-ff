6eb7fdb7b5bc3288f5840f988a5ddcf1da5bca5c
Maksim Mezenets <makc67891@yandex.ru>
2024-03-10 18:05:29 +0300

    upd

--- scripts/index.js
+++ scripts/index.js
@@ -6,24 +6,24 @@ const content = document.querySelector(".content");
 const placesList = content.querySelector(".places__list");
 
 // @todo: Функция создания карточки
-function addPlacesItem(name, link) {
+function createPlacesItem(name, link) {
   const placesItem = cardTemplate.querySelector(".places__item").cloneNode(true);
-  placesItem.querySelector(".card__image").src = link;
-  placesItem.querySelector(".card__image").alt = name;
+  const cardImage = placesItem.querySelector(".card__image");
+  cardImage.src = link;
+  cardImage.alt = name;
   placesItem.querySelector(".card__title").textContent = name;
-  placesItem.querySelector(".card__delete-button").addEventListener("click", () => resetPlacesItem(placesItem));
-
-  placesList.append(placesItem);
+  placesItem.querySelector(".card__delete-button").addEventListener("click", () => deletePlacesItem(placesItem));
+  return placesItem;
 }
 
 // @todo: Функция удаления карточки
-function resetPlacesItem(elem) {
+function deletePlacesItem(elem) {
   elem.remove();
 }
 
 // @todo: Вывести карточки на страницу
 function renderPlacesItem() {
-  initialCards.forEach(elem => addPlacesItem(elem.name, elem.link));
+  initialCards.forEach(elem => placesList.append(createPlacesItem(elem.name, elem.link)));
 }
 
-renderPlacesItem();
+renderPlacesItem(); 
