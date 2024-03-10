e87640c6987434a8f97622f207688ddc988890d3
Maksim Mezenets <makc67891@yandex.ru>
2024-03-10 20:01:10 +0300

    upd

--- scripts/index.js
+++ scripts/index.js
@@ -6,13 +6,13 @@ const content = document.querySelector(".content");
 const placesList = content.querySelector(".places__list");
 
 // @todo: Функция создания карточки
-function createPlacesItem(name, link) {
+function createPlacesItem(elem, functionDelete) {
   const placesItem = cardTemplate.querySelector(".places__item").cloneNode(true);
   const cardImage = placesItem.querySelector(".card__image");
-  cardImage.src = link;
-  cardImage.alt = name;
-  placesItem.querySelector(".card__title").textContent = name;
-  placesItem.querySelector(".card__delete-button").addEventListener("click", () => deletePlacesItem(placesItem));
+  cardImage.src = elem.link;
+  cardImage.alt = elem.name;
+  placesItem.querySelector(".card__title").textContent = elem.name;
+  placesItem.querySelector(".card__delete-button").addEventListener("click", () => functionDelete(placesItem));
   return placesItem;
 }
 
@@ -23,7 +23,7 @@ function deletePlacesItem(elem) {
 
 // @todo: Вывести карточки на страницу
 function renderPlacesItem() {
-  initialCards.forEach(elem => placesList.append(createPlacesItem(elem.name, elem.link)));
+  initialCards.forEach(elem => placesList.append(createPlacesItem(elem, deletePlacesItem)));
 }
 
 renderPlacesItem(); 
